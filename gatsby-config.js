9255c548cb96631539e984530a0125e20b7cd75a
AnuragReddy2000 <anuragreddy1000@gmail.com>
2022-02-03 10:30:31 +0530

    Changes the manifest icons section

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,56 @@ module.exports = {
         theme_color: "#fafafa",
         display: "standalone",
         icon: "src/images/icon.png",
+        icons: [
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi48.png",
+            "sizes":"48x48",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi72.png",
+            "sizes":"72x72",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi96.png",
+            "sizes":"96x96",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi144.png",
+            "sizes":"144x144",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi192.png",
+            "sizes":"192x192",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi256.png",
+            "sizes":"256x256",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi384.png",
+            "sizes":"384x384",
+            "type":"image/png"
+          },
+          {
+            "purpose":"any maskable",
+            "src":"webAppIcons/logoi512.png",
+            "sizes":"512x512",
+            "type":"image/png"
+          }
+        ],
         icon_options: {
           purpose: `any maskable`,
         },
